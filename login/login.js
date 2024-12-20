68a1fb37b697019612ea5311383aff4d74d37139
Rafael Andrade <rafaelreis2011@gmail.com>
2024-12-20 10:12:41 -0500

    add login e register

---
+++ login/login.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".switch-form a").forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = this.getAttribute("href").substring(1);
+      document.querySelectorAll(".form-container").forEach(form => {
+        form.classList.add("hidden");
+      });
+      document.getElementById(target).classList.remove("hidden");
+    });
+  });
